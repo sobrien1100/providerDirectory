87deedde68803cb38000be4f1390ff82d6ba5591
Sean O'Brien <sobrien1100gmail.com>
2022-04-14 16:49:55 -0400

    Initial commit

---
+++ src/main/java/com/updox/providerdirectory/ProviderDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.updox.providerdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderDirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderDirectoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/updox/providerdirectory/ProviderDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.updox.providerdirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderDirectoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
